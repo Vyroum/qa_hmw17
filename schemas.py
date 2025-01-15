cc157af5ef2074457c16cb08ee5356f42abfb7b3
Andrei Monichev <vyroud@gmail.com>
2025-01-15 15:14:15 +0300

    git init

---
+++ schemas.py
@@ -0,0 +1,179 @@
+get_user = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "properties": {
+        "data": {
+            "type": "object",
+            "properties": {
+                "id": {
+                    "type": "integer"
+                },
+                "email": {
+                    "type": "string"
+                },
+                "first_name": {
+                    "type": "string"
+                },
+                "last_name": {
+                    "type": "string"
+                },
+                "avatar": {
+                    "type": "string"
+                }
+            },
+            "required": [
+                "id",
+                "email",
+                "first_name",
+                "last_name",
+                "avatar"
+            ]
+        },
+        "support": {
+            "type": "object",
+            "properties": {
+                "url": {
+                    "type": "string"
+                },
+                "text": {
+                    "type": "string"
+                }
+            },
+            "required": [
+                "url",
+                "text"
+            ]
+        }
+    },
+    "required": [
+        "data",
+        "support"
+    ]
+}
+
+post_user = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "properties": {
+        "name": {
+            "type": "string"
+        },
+        "job": {
+            "type": "string"
+        },
+        "id": {
+            "type": "string"
+        },
+        "createdAt": {
+            "type": "string"
+        }
+    },
+    "required": [
+        "name",
+        "job",
+        "id",
+        "createdAt"
+    ]
+}
+
+update_user = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "properties": {
+        "name": {
+            "type": "string"
+        },
+        "job": {
+            "type": "string"
+        },
+        "updatedAt": {
+            "type": "string"
+        }
+    },
+    "required": [
+        "updatedAt"
+    ]
+}
+
+register_user = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "id": {
+      "type": "integer"
+    },
+    "token": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "id",
+    "token"
+  ]
+}
+
+error_register_user = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "error": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "error"
+  ]
+}
+
+get_resource = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "data": {
+      "type": "object",
+      "properties": {
+        "id": {
+          "type": "integer"
+        },
+        "name": {
+          "type": "string"
+        },
+        "year": {
+          "type": "integer"
+        },
+        "color": {
+          "type": "string"
+        },
+        "pantone_value": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "id",
+        "name",
+        "year",
+        "color",
+        "pantone_value"
+      ]
+    },
+    "support": {
+      "type": "object",
+      "properties": {
+        "url": {
+          "type": "string"
+        },
+        "text": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "url",
+        "text"
+      ]
+    }
+  },
+  "required": [
+    "data",
+    "support"
+  ]
+}
---
+++ test.py
@@ -0,0 +1,53 @@
+from logging import raiseExceptions
+
+import requests
+from jsonschema import validate
+
+
+from schemas import get_user, post_user, update_user, register_user, error_register_user
+
+
+def test_create_user():
+    response = requests.post("https://reqres.in/api/users", data={"name": "morpheus", "job": "master"})
+
+    assert response.status_code == 201
+    body = response.json()
+    validate(body, post_user)
+
+
+def test_get_user_info_positive():
+    response = requests.get("https://reqres.in/api/users/2")
+
+    assert response.status_code == 200
+    body = response.json()
+    validate(body, get_user)
+
+
+def test_get_user_info_negative():
+    response = requests.get("https://reqres.in/api/users/23")
+
+    assert response.status_code == 404
+
+
+def test_update_user_info():
+    response = requests.put("https://reqres.in/api/users/2", data={"name": "Trinity", "job": "bodyguard"})
+    assert response.status_code == 200
+    body = response.json()
+    validate(body, update_user)
+
+
+def test_successful_user_registration():
+    response = requests.post("https://reqres.in/api/register",
+                             data={"email": "eve.holt@reqres.in", "password": "pistol"})
+    assert response.status_code == 200
+    body = response.json()
+    validate(body, register_user)
+
+
+def test_unsuccessful_user_registration():
+    response = requests.post("https://reqres.in/api/register",
+                             data={"email": "sydney@fife"})
+    assert response.status_code == 400
+    body = response.json()
+    validate(body, error_register_user)
+
